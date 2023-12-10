123393c68c9d19506660d33c7f97ed4606c6533f
felipespas <151067779+felipespas@users.noreply.github.com>
2023-12-10 15:11:37 -0300

    adjusts

--- _MyScripts/1-deploy.bash
+++ _MyScripts/1-deploy.bash
@@ -6,9 +6,11 @@ az account set --subscription "2edd29f5-689f-48c5-b93e-93723216ea91"
 
 az bicep build --file azuredeploy.bicep
 
-$resourceGroupName="mslearn-cosmos-openai5"
+$resourceGroupName="mslearn-cosmos-openai6"
 
 az group create --name $resourceGroupName --location "eastus"
+
+# do not forget to commit and sync with the remote repo
 
 az deployment group create `
     --resource-group $resourceGroupName `
